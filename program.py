77708c4ab5bddd5b47f4a9019753a8e76db23249
Bert Bullough <bert@bertbullough.com>
2018-06-29 09:18:40 -0600

    Initial commit

---
+++ program.py
@@ -0,0 +1,10 @@
+def header()
+    print("------------------------")
+    print("   Movie Search App")
+    print("------------------------")
+
+    header()
+    pass
+
+if __name__ == '__main__':
+    main()
