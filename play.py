c450e0a1b2ff082e004d9662d0cb01a13a894cf3
Bert Bullough <bert@bertbullough.com>
2018-06-29 15:32:12 -0600

    search working

--- play.py
+++ play.py
@@ -6,13 +6,30 @@ MovieResult = collections.namedtuple(
     "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres"
 )
 
-search = 'capital'
+search = 'runner'
 url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
 
 resp = requests.get(url)
 resp.raise_for_status()
 
 movie_data = resp.json()
-movies = movie_data.get('hits')
+movies_list = movie_data.get('hits')
 
-print(type(movie_data), movies)
+movies = []
+for md in movies_list:
+    m = MovieResult(
+        imdb_code=md.get('imdb_code'),
+        title=md.get('title'),
+        duration=md.get('duration'),
+        director=md.get('director'),
+        year=md.get('year', 0),
+        rating=md.get('year', 0),
+        imdb_score=md.get('imdb_score', 0.0),
+        keywords=md.get('keywords'),
+        genres=md.get('genres')
+    )
+    movies.append(m)
+
+print("Found {} movies for search {}".format(len(movies), search))
+for m in movies:
+    print("{} -- {} ".format(m.year, m.title))
