6196e39fb41c641dd6d3f48a323ee642328bb6dd
Bert Bullough <bert@bertbullough.com>
2018-06-29 15:34:05 -0600

    renamed

--- program.py
+++ program.py
@@ -1,11 +1,35 @@
-def header()
-    print("------------------------")
-    print("   Movie Search App")
-    print("------------------------")
+import requests
+import collections
 
-    header()
-    pass
+MovieResult = collections.namedtuple(
+    'MovieResult',
+    "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres"
+)
 
+search = input("What movie do you want to search for?")
+url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
 
-if __name__ == '__main__':
-    main()
+resp = requests.get(url)
+resp.raise_for_status()
+
+movie_data = resp.json()
+movies_list = movie_data.get('hits')
+
+movies = []
+for md in movies_list:
+    m = MovieResult(
+        imdb_code=md.get('imdb_code'),
+        title=md.get('title'),
+        duration=md.get('duration'),
+        director=md.get('director'),
+        year=md.get('year', 0),
+        rating=md.get('year', 0),
+        imdb_score=md.get('imdb_score', 0.0),
+        keywords=md.get('keywords'),
+        genres=md.get('genres')
+    )
+    movies.append(m)
+
+print("Found {} movies for search {}".format(len(movies), search))
+for m in movies:
+    print("{} -- {} ".format(m.year, m.title))
