1dd1e58a1faf393e30ceba260db77d42a0f17d42
Bert Bullough <bert@bertbullough.com>
2018-07-01 13:47:03 -0600

    defined movie_svc function

---
+++ movie_svc.py
@@ -0,0 +1,24 @@
+import requests
+import collections
+
+MovieResult = collections.namedtuple(
+    'MovieResult',
+    "imdb_code,title,duration,director,year,rating,imdb_score,keywords,genres"
+)
+
+
+def find_movies(search_text):
+    url = 'http://movie_service.talkpython.fm/api/search/{}'.format(search)
+
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    movie_data = resp.json()
+    movies_list = movie_data.get('hits')
+
+    movies = [
+        MovieResult(**md)
+        for md in movies_list
+    ]
+
+    return movies
